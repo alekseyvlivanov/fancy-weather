33c754bccc65ee841de0f8943183e1f449dd966b
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-05-28 17:35:27 +1000

    feat: add weather icons

--- fancy-weather/src/components/weather/weather.js
+++ fancy-weather/src/components/weather/weather.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import today from '../../assets/today.svg';
-
 import './weather.css';
 
 function Weather(props) {
@@ -29,7 +27,11 @@ function Weather(props) {
           <span className="left-degrees">°</span>
         </div>
         <div className="today-right">
-          <img className="today-icon" src={today} alt="today icon" />
+          <img
+            className="today-icon"
+            src={`../assets/${current.weather.icon}.png`}
+            alt="today icon"
+          />
           <div className="today-summary">
             <p className="summary">{current.weather.description}</p>
             <p className="feels">
@@ -51,21 +53,33 @@ function Weather(props) {
           <h5>{props.dtF1}</h5>
           <div className="day-summary">
             <span>{forecast1.temp}°</span>
-            <img className="day-icon" src={today} alt="day icon" />
+            <img
+              className="day-icon"
+              src={`../assets/${forecast1.weather.icon}.png`}
+              alt="day icon"
+            />
           </div>
         </div>
         <div className="weather-day">
           <h5>{props.dtF2}</h5>
           <div className="day-summary">
             <span>{forecast2.temp}°</span>
-            <img className="day-icon" src={today} alt="day icon" />
+            <img
+              className="day-icon"
+              src={`../assets/${forecast2.weather.icon}.png`}
+              alt="day icon"
+            />
           </div>
         </div>
         <div className="weather-day">
           <h5>{props.dtF3}</h5>
           <div className="day-summary">
             <span>{forecast3.temp}°</span>
-            <img className="day-icon" src={today} alt="day icon" />
+            <img
+              className="day-icon"
+              src={`../assets/${forecast3.weather.icon}.png`}
+              alt="day icon"
+            />
           </div>
         </div>
       </div>
